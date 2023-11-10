c1d9ed8a9757746237b0d3208a1db91843b7ce29
Blair Currey <12960453+BlairCurrey@users.noreply.github.com>
2023-11-09 20:27:35 -0500

    feat: add pegs

--- README.md
+++ README.md
@@ -14,6 +14,8 @@ Can test build by running `dist/index.html` on a local server (such as VScode li
   - not sure about game.js.map and game.js (why does it look different than dev/watch output?)
 - [ ] ball sprite/logic should be seperated from scene (make own class?)
 - [ ] new scene dir with Preloader and Game. needs to be supported in rollup (currently only supports output of src/game.ts specifically)
+- [ ] make pegs smaller (but make sure its visible)
+- [ ] fix peg hitbox to match sprite
 
 ## alpha features
 
@@ -22,14 +24,17 @@ Can test build by running `dist/index.html` on a local server (such as VScode li
 - [x] aimer controlled with keyboard
 - [x] ball shoots out on cannon fire
 - [x] ball is subject to gravity
-- [ ] some arbitrary number of pegs/blocks on screen (some trivial level)
-- [ ] ball bounces off pegs/blocks in realistic way
+- [x] some arbitrary number of pegs/blocks on screen (some trivial level)
+- [x] ball bounces off pegs/blocks
+- [ ] better physics: ball should lose momentum on peg hit
 - [x] ball bounces off walls
-- [ ] objects disappear when hit by ball
+- [x] objects disappear when hit by ball
+- [x] add delay to objects disappearing when hit by ball
 - [ ] ball is "gone" when it hits bottom of screen
 - [ ] when ball is "gone" a new ball loads up top (and cannot shoot before this point)
 - [ ] there is some ball limit (3, 5?) and game over when limit is 0 and turn is over
 - [x] "Preloader" scene to preload all assets, then "Game" scene https://youtu.be/z15L4E7A3wY?si=HRf457MpaxG0EbKl&t=318
+- [ ] free ball bucket - moving "bucket" that should end turn but not decrease ball possession
 
 ## beta featrures
 
@@ -38,5 +43,4 @@ Can test build by running `dist/index.html` on a local server (such as VScode li
 - [ ] audio
   - [ ] on shoot
   - [ ] on block hit
-- [ ] add delay to objects disappearing when hit by ball
 - [ ] HUD: score, ball count
--- src/game.ts
+++ src/game.ts
@@ -7,6 +7,10 @@ class Preloader extends Phaser.Scene {
 
   preload() {
     this.load.image("ball", "assets/ball.png");
+    this.load.image("peg1", "assets/peg1.png");
+    this.load.image("peg2", "assets/peg2.png");
+    this.load.image("peg3", "assets/peg3.png");
+    this.load.image("peg4", "assets/peg4.png");
   }
 
   create() {
@@ -20,6 +24,36 @@ class Game extends Phaser.Scene {
   }
 
   create() {
+    const makePeg = (
+      pegs: Phaser.Physics.Arcade.Group,
+      x: number,
+      y: number,
+      texture: string
+    ) => {
+      const peg = pegs.create(x, y, texture);
+      // const peg = this.physics.add.sprite(x, y, texture);
+      const pegWidth = 20;
+      peg.setScale(pegWidth / peg.width, pegWidth / peg.height);
+      peg.body.setCircle(pegWidth);
+      peg.setImmovable(true);
+      return peg;
+    };
+    // Make pegs
+    const pegs = this.physics.add.group();
+
+    makePeg(pegs, 300, 300, "peg2");
+    makePeg(pegs, 215, 350, "peg2");
+
+    // const peg = pegs.create(300, 300, "peg2");
+    // const pegWidth = 20;
+    // peg.setScale(pegWidth / peg.width, pegWidth / peg.height);
+    // peg.body.setCircle(pegWidth);
+    // peg.setImmovable(true);
+    // const peg2 = pegs.create(215, 350, "peg2");
+    // peg2.setScale(pegWidth / peg.width, pegWidth / peg.height);
+    // peg2.body.setCircle(pegWidth);
+    // peg2.setImmovable(true);
+
     // TODO: make this repeatable so we can reset ball on end of turn
     const centerX = this.cameras.main.width / 2;
     const ballWidth = 15;
@@ -27,7 +61,28 @@ class Game extends Phaser.Scene {
     const ballStarty = ballWidth + 1;
     const ball = this.physics.add.sprite(ballStartX, ballStarty, "ball");
     ball.setScale(ballWidth / ball.width, ballWidth / ball.height);
+    ball.body.setCircle(ballWidth * 2);
     ball.setBounce(1).setCollideWorldBounds(true);
+    // TODO: best way to do this?
+    ball.setDrag(0.99);
+
+    const handleCollision = (ball, peg) => {
+      // Add a 2-second delay before destroying the peg using setTimeout
+      setTimeout(() => {
+        // Create a fade-out animation for the peg
+        this.tweens.add({
+          targets: peg,
+          alpha: 0, // Make the peg fully transparent
+          duration: 1000, // Duration of the animation in milliseconds
+          onComplete: () => {
+            // Once the animation is complete, destroy the peg
+            peg.destroy();
+          },
+        });
+      }, 2000);
+    };
+
+    this.physics.add.collider(ball, pegs, handleCollision);
 
     // Create a line for aiming
     const aimingLine = this.add.graphics();
@@ -84,6 +139,13 @@ class Game extends Phaser.Scene {
 
     updateAim(); // Initial update of aiming line
   }
+
+  update() {
+    // Phaser.Physics.Arcade.collide("ball", "peg2", (ball, peg) => {
+    //   peg.destroy();
+    // }
+    // collide
+  }
 }
 
 const config: Phaser.Types.Core.GameConfig = {
@@ -95,6 +157,7 @@ const config: Phaser.Types.Core.GameConfig = {
   physics: {
     default: "arcade",
     arcade: {
+      debug: true,
       // alternatively, set in scene create with this.physics.world.setBounds
       checkCollision: {
         up: true,
